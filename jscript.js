5e30751c52b025257c9e9d48dde8ceaa63f2b447
kgawerda <konradgawerda@gmail.com>
2022-11-11 17:21:52 +0100

    do playTurn

--- jscript.js
+++ jscript.js
@@ -1,5 +1,9 @@
 const Gameboard = (() => {
 
+    const Player = (name, sign, turn) => {
+        return { name, sign, turn };
+    };
+
     const playerX = Player('playerX', 'X', true);
     const playerO = Player('playerO', 'O', false);
 
@@ -16,16 +20,33 @@ const Gameboard = (() => {
 
     let board=[];
     let winner=null;
+    let turn=1; //0-O 1-X
+
+    function playTurn(e){
+        if(e.target.textContent=='' && playerX.turn && Gameboard.winner==null){
+            console.log('x');
+            board[e.target.id]=playerX.sign;
+            e.target.textContent=playerX.sign;
+            playerX.turn=false;
+            playerO.turn=true;
+        }
+        else if(e.target.textContent=='' && playerO.turn && Gameboard.winner==null){
+            console.log('o');
+            board[e.target.id]=playerO.sign;
+            e.target.textContent=playerO.sign;
+            playerO.turn=false;
+            playerX.turn=true;
+        }
+    }
 
 
+    const box=Array.from(document.querySelectorAll('.square'));
+    box.forEach(box => box.addEventListener('click', playTurn));
 
-
-
+    return{winner, playTurn};
 })();
 
-const Player = (name, sign, turn) => {
-    return { name, sign, turn };
-};
+
 
 const displayController = (() => {
 
